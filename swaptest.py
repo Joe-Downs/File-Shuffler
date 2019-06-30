0d42cb1ddfb86ba652cd5238f09d177088f612c9
Joe Downs <joedowns502@gmail.com>
2019-06-30 01:11:14 -0400

    Swaptest: Initial commit

---
+++ swaptest.py
@@ -0,0 +1,22 @@
+import os
+directory_input = input("Choose the Folder to Shuffle: ")
+directory = os.fsencode(directory_input)
+
+print("\nPlease choose the extension(s) you wish to be swapped.\nPress enter to exit after you have finished input.\n")
+
+swapped_extensions = []
+answer = " "
+
+#Prompt reappears after each input until the user hits enter on the blank field
+while answer != "":
+    answer = input("Which extensions do you want swapped? ")
+    if answer != "":
+        swapped_extensions.append(answer)
+
+print(swapped_extensions)
+
+#Empty arrays to preserve original filenames for later renaming of new files after move
+folder_one_filenames   = []
+folder_two_filenames   = []
+folder_three_filenames = []
+
