d9471cbb3b024110b24b8897f283613a31afcef0
Joe Downs <joedowns502@gmail.com>
2019-07-06 23:04:04 -0400

    swaptest: add recursion for file_sort() function

--- swaptest.py
+++ swaptest.py
@@ -17,34 +17,29 @@ print("Chosen Extensions: " + str(swapped_extensions))
 
 
 #For each folder and file in a directory, adds full path to either folder array or file array
-
-dir_files  = os.listdir(directory_string)
-dir_array  = []
+dir_array  = [directory_string]
 file_array = []
-print("Directory Files: " + str(dir_files))
 
 #Need to have input for which directory to sort
-def file_sort():
+def file_sort(target_dir):
+    dir_files  = os.listdir(target_dir)
+   #print("Directory Files: " + str(dir_files))
     for entry in dir_files:
-        if directory_string.endswith("/"):
-            string_filepath = str(directory_string + entry)
+        if target_dir.endswith("/"):
+            string_filepath = str(target_dir + entry)
         else:
-            string_filepath = str(directory_string + "/" + entry)
+            string_filepath = str(target_dir + "/" + entry)
             
-        if os.path.isdir(string_filepath): #If the path is a directory, add path to dir_array
+        if os.path.isdir(string_filepath):      #If the path is a directory, add path to dir_array
             dir_array.append(string_filepath)
-        elif os.path.isfile(string_filepath):#If the path is a file, add path to file_array
+        elif os.path.isfile(string_filepath):   #If the path is a file, add path to file_array
             file_array.append(string_filepath)
-file_sort()
-
-print("Directory Array: " + str(dir_array))
-print("File Array: " + str(file_array))
 
 while dir_array != []:
     for directory in dir_array:
-        print(directory)
-        file_sort()
+        file_sort(target_dir = directory)
         dir_array.remove(directory)
-        print(dir_array)
-        
-        
+
+print("Directory Array Final: " + str(dir_array))
+print("Directory Array: " + str(dir_array))
+print("File Array: " + str(file_array))
